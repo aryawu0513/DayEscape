6bb3ac98a8f8a17c07bdfe8f8a425c833ffccbab
Lian Liu <lilianliu@Lians-MacBook-Pro.local>
2023-11-29 23:01:17 -0500

    add fake note

---
+++ fake_note.js
@@ -0,0 +1,14 @@
+export const fakeNote = [
+  {
+    place: "Trader Joe's",
+    related_route: "2023-11-27T12:01:30",
+    note_description:
+      "Shopping list:\nGrapefruit soda\nCrackling Candle\nTruffle Cheese",
+  },
+  {
+    place: "Trader Joe's",
+    related_route: null,
+    note_description:
+      "Shopping list:\nGrapefruit soda\nChocolate Croissant Candle\nGouda",
+  },
+]
