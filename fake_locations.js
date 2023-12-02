c56333192faa3ec5e214785fe7d77cd58b73db49
Lian Liu <lilianliu@Lians-MacBook-Pro.local>
2023-12-02 13:36:16 -0500

    added locations fake data

---
+++ fake_locations.js
@@ -0,0 +1,44 @@
+export const locations = [
+    {
+        name: "MFA Museum",
+        coordinates: {
+          latitude: 42.34009834595002,
+          longitude: -71.09354256326445,
+        },
+    },
+    {
+        name: "Trader Joe's",
+        coordinates: {
+          latitude: 42.3489134659583,
+          longitude: -71.08397537531168,
+        },
+    },
+    {
+        name: "GoodWill",
+        coordinates: {
+          latitude: 42.364363276961264,
+          longitude: -71.10206665252885,
+        },
+    },
+    {
+        name: "Isabella Stewart Gardner Museum",
+        coordinates: {
+          latitude: 42.33831306309043,
+          longitude: -71.09905979797081,
+        },
+    },
+    {
+        name: "Eataly",
+        coordinates: {
+          latitude: 42.34782318454349,
+          longitude: -71.08241543323422,
+        },
+    },
+    {
+        name: "H Mart",
+        coordinates: {
+          latitude: 42.36762456990014, 
+          longitude: -71.10243129412719,
+        },
+    }
+]
